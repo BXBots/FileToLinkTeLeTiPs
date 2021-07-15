0f9cfa0821dba599ac9579a293c820da8278769d
BXBots <86224222+BXBots@users.noreply.github.com>
2021-07-15 16:05:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ def short(url):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hai,\n\n>> `Iam a simple File To Link Bot.send me any file I will give you short link`.\n\n👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : @BX_Botz'
+    bot.reply_to(message, 'Hai,\n\n>> `Iam a simple File To Link Bot.send me any file I will give you short link`.\n\n👲 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : @BX_Botz')
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
